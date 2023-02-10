cc8f9535f70876d42b9ccbab04d35abd7b068c18
Marcus Nogueira <marcusnogueiraa@gmail.com>
2023-02-10 17:45:47 -0300

    New Exercices - Beecrowd 01
    Add C 1001 Exercice

---
+++ 01 Introdução/1001 Extremamente Básico/1001.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  int a, b;
+  scanf("%d", &a);
+  scanf("%d", &b);
+  printf("X = %d\n", a + b);
+}
+
+// https: // www.beecrowd.com.br/judge/pt/problems/view/1002
+// Ranking 114561º, Time 0.000, Submiss Date 24/03/2022 14:39:51 (C99)
