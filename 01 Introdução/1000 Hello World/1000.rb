8d3a03a98a4784858c3fe7fd717cc65338a5c783
Marcus Nogueira <marcusnogueiraa@gmail.com>
2023-02-26 03:59:41 -0300

    New Exercices - Beecrowd 01
    Add Ruby 1000 Exercice
    Add Python 1059 Exercice

---
+++ 01 Introdução/1000 Hello World/1000.rb
@@ -0,0 +1,4 @@
+puts "Hello World!"
+
+# https://www.beecrowd.com.br/judge/pt/problems/view/1000
+# Ranking 00986º, Time 0.377, Submiss Date 26/02/2023 03:45:12 (Ruby 2.7)
---
+++ 01 Introdução/1059 Numeros Pares/1059.py
@@ -0,0 +1,5 @@
+for num in range(2, 101, 2):
+    print(num)
+
+# https://www.beecrowd.com.br/judge/pt/problems/view/1059
+# Ranking 14923º, Time 0.084, Submiss Date 25/02/2023 16:50:38 (Python 3.9)
