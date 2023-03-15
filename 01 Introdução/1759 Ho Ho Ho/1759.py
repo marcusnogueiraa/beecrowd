3bb32bb829be495ba90fd5f25ee0d544c5d2adce
Marcus Nogueira <marcusnogueiraa@gmail.com>
2023-03-15 20:58:01 -0300

    New Exercices - Beecrowd 01
    Add Python 1759 Exercice

---
+++ 01 Introdução/1759 Ho Ho Ho/1759.py
@@ -0,0 +1,10 @@
+num = int(input())
+
+for i in range(0, num):
+    if i == num-1:
+        print('Ho!')
+    else:
+        print('Ho', end=' ')
+
+# https://www.beecrowd.com.br/judge/pt/problems/view/1759
+# Ranking 03177º, Time 0.593, Submiss Date 14/03/2023 16:24:20 (Python 3.9)
