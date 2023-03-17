47ab3400d399fb86edd40b294305bcdb72d93bc2
Marcus Nogueira <marcusnogueiraa@gmail.com>
2023-03-16 21:25:18 -0300

    New Exercices - Beecrowd 01
    Add Python 1930 Exercice

---
+++ 01 Introdução/1930 Tomadas/1930.py
@@ -0,0 +1,4 @@
+print(sum([int(x) for x in input().split(' ')]) - 3)
+
+# https://www.beecrowd.com.br/judge/pt/problems/view/1930
+# Ranking 01807º, Time 0.024, Submiss Date 16/03/2023 20:43:32 (Python 3.9)
